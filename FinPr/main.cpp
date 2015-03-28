6ca1621980b3b37cc7de2697b1d70cd85f03753f
liuweifengzju <liuweifengstat@gmail.com>
2015-03-28 10:25:34 -0400

    Add usingnamespace

--- FinPr/main.cpp
+++ FinPr/main.cpp
@@ -8,8 +8,9 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[]) {
-	std::cout << "My name is Weifeng Liu.";
-	std::cout << "Hello, World!\n";
+	cout << "Hello, World!\n";
     return 0;
 }
