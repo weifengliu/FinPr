b04a9710077cebd0c8f66a80c5fa7ea22dcf4f18
liuweifengzju <liuweifengstat@gmail.com>
2015-04-01 23:07:59 -0400

    Create Property pattern.

---
+++ FinPr/property.cpp
@@ -0,0 +1,101 @@
+//
+//  property.cpp
+//  FinPr
+//
+//  Created by Weifeng Liu on 4/1/15.
+//  Copyright (c) 2015 Weifeng Liu. All rights reserved.
+//
+
+#ifndef PROPERTY_CPP
+#define PROPERTY_CPP
+
+#include "property.hpp"
+
+template <class Name, class Value>
+Property<Name, Value> :: Property()
+{
+	// Constructor
+	nam = Name();
+	val = Value();
+}
+
+template <class Name, class Value>
+Property<Name, Value> :: Property(const Name& name)
+{
+	// Constructor
+	nam = name;
+	val = Value();
+}
+
+template <class Name, class Value>
+Property<Name, Value> :: Property(const Value& v)
+{
+	// Constructor
+	nam = Name();
+	val = v;
+}
+
+template <class Name, class Value>
+Property<Name, Value> :: Property(const Name& name, const Value& v)
+{
+	// Constructor
+	nam = name;
+	val = v;
+}
+
+template <class Name, class Value>
+Property<Name, Value> :: Property(const Property<Name, Value>& source)
+{
+	// Constructor
+	nam = source.name;
+	val = source.v;
+}
+
+template <class Name, class Value>
+Property<Name, Value> :: ~Property()
+{
+	// Destructor
+}
+
+template <class Name, class Value>
+Property<Name, Value>& Property<Name, Value> :: operator = (const Property<Name, Value>& source)
+{
+	nam = source.nam;
+	val = source.val;
+	return *this;
+}
+
+template <class Name, class Value>
+bool Property<Name, Value> :: operator == (const Property<Name, Value>& prop2)
+{
+	if (nam != prop2.nam) return false;
+	if (val != prop2.val) return false;
+	return true;
+}
+
+template <class Name, class Value>
+Name Property<Name, Value> :: name() const
+{
+	return nam;
+}
+
+template <class Name, class Value>
+Value Property<Name, Value> :: operator ()() const
+{
+	return val;
+}
+
+template <class Name, class Value>
+void Property<Name, Value> :: name(const Name& new_name)
+{
+	nam = new_name;
+}
+
+template <class Name, class Value>
+void Property<Name, Value> :: operator ()(const Value& new_val)
+{
+	val = new_val;
+}
+
+
+#endif
---
+++ FinPr/property.hpp
@@ -0,0 +1,43 @@
+//
+//  property.hpp
+//  FinPr
+//
+//  Base class for Properties.
+//
+//  Created by Weifeng Liu on 3/28/15.
+//  Copyright (c) 2015 Weifeng Liu. All rights reserved.
+//
+
+#ifndef PROPERTY_HPP
+#define PROPERTY_HPP
+
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+template <class Name = string, class Value = double> class Property
+{
+private:
+	Name nam;
+	Value val;
+	Property<Name, Value>& operator = (const Property<Name, Value>& source);
+	
+public:
+	Property();
+	Property(const Name& name);
+	Property(const Value& v);
+	Property(const Name& name, const Value& v);
+	Property(const Property<Name, Value>& source);
+	
+	virtual ~Property();
+	
+	virtual Name name() const;
+	virtual Value operator ()() const;
+	virtual void name(const Name& new_name);
+	virtual void operator ()(const Value& new_val);
+	
+	bool operator == (const Property<Name, Value>& prop2);
+};
+
+#endif
